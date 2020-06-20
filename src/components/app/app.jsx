c4830cb3ab346edd073751bf20f923d9d1bc757b
Anton Churkin <afrg@yandex.ru>
2020-06-20 23:59:39 +0500

    connecting Router to App component

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,17 +1,42 @@
-import React from 'react';
-import Main from './../main/main.jsx';
+import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import Main from './../main/main.jsx';
+import Property from '../property/property.jsx';
 
-const handleApartmentTitleClick = () => {};
+class App extends PureComponent {
+  constructor(props) {
+    super(props);
 
-const App = (props) => {
-  const {apartmentList} = props;
+    this.handleApartmentTitleClick = this.handleApartmentTitleClick.bind(this);
+  }
 
-  return <Main
-    apartmentList={apartmentList}
-    onApartmentTitleClick={handleApartmentTitleClick}
-  />;
-};
+  handleApartmentTitleClick() {
+    // TODO доделать метод
+  }
+
+  _renderMainScreen() {
+    const {apartmentList} = this.props;
+
+    return <Main
+      apartmentList={apartmentList}
+      onApartmentTitleClick={this.handleApartmentTitleClick}
+    />;
+  }
+
+  render() {
+    return <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          {this._renderMainScreen()}
+        </Route>
+        <Route exact path="/dev-property">
+          <Property/>
+        </Route>
+      </Switch>
+    </BrowserRouter>;
+  }
+}
 
 App.propTypes = {
   apartmentList: PropTypes.arrayOf(PropTypes.shape()).isRequired
