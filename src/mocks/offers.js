c4b7a2fb15ec48e262730a6bf4afd95b53f676eb
Anton Churkin <afrg@yandex.ru>
2020-06-20 16:22:12 +0500

    add mocks

---
+++ src/mocks/offers.js
@@ -0,0 +1,42 @@
+export default [
+  {
+    id: 1,
+    type: `Apartment`,
+    description: `Beautiful & luxurious apartment at great location`,
+    rating: 4.1,
+    price: 120.0,
+    isPremium: true,
+    isFavourite: true,
+    photo: `img/room.jpg`
+  },
+  {
+    id: 2,
+    type: `Apartment`,
+    description: `Canal View Prinsengracht`,
+    rating: 3.7,
+    price: 80.0,
+    isPremium: false,
+    isFavourite: false,
+    photo: `img/apartment-01.jpg`
+  },
+  {
+    id: 3,
+    type: `Private room`,
+    description: `Wood and stone place`,
+    rating: 5,
+    price: 132.0,
+    isPremium: false,
+    isFavourite: true,
+    photo: `img/apartment-02.jpg`
+  },
+  {
+    id: 4,
+    type: `Apartment`,
+    description: `Nice, cozy, warm big bed apartment`,
+    rating: 4.7,
+    price: 180.0,
+    isPremium: false,
+    isFavourite: true,
+    photo: `img/apartment-03.jpg`
+  },
+];
