23dc5bd607bd62aba4885ae50b9df66bd559b859
BruTyler <afrg@yandex.ru>
2020-06-21 21:43:11 +0500

    Личный проект: таинственные подробности (#7)
    * add propTypes
    
    * add package router-dom
    
    * add component Property
    
    * connecting Router to App component
    
    * Component Property is ready, TitleClick is updated
    
    * Property - snapshot test is ready
    
    * add e2e test of TitleClick
    
    * Update src/components/property/property.jsx
    
    Co-authored-by: Ruslan Malogulko <ruslan.malogulko@verygoodsecurity.com>
    
    * fix: naming convention in tests
    
    Co-authored-by: Ruslan Malogulko <ruslan.malogulko@verygoodsecurity.com>

--- src/components/apartment-card/apartment-card.e2e.test.js
+++ src/components/apartment-card/apartment-card.e2e.test.js
@@ -2,24 +2,25 @@ import React from 'react';
 import {shallow} from 'enzyme';
 import ApartmentCard from './apartment-card.jsx';
 
+const SINGLE_APARTMENT = {
+  id: 0,
+  type: `Apartment`,
+  description: `description0`,
+  rating: 0.1,
+  price: 1,
+  isPremium: true,
+  isFavourite: true,
+  photo: `img0.jpg`
+};
+
 describe(`<ApartmentCard /> e2e suite`, () => {
   it(`<ApartmentCard /> Card send apartment info on mouse enter`, () => {
     const onApartmentCardHoverMock = jest.fn();
     const onApartmentTitleClickMock = jest.fn();
-    const apartmentMock = {
-      id: 0,
-      type: `Apartment`,
-      description: `description0`,
-      rating: 0.1,
-      price: 1,
-      isPremium: true,
-      isFavourite: true,
-      photo: `img0.jpg`
-    };
 
     const apartmentCardWrapper = shallow(
         <ApartmentCard
-          apartment={apartmentMock}
+          apartment={SINGLE_APARTMENT}
           onApartmentCardHover={onApartmentCardHoverMock}
           onApartmentTitleClick={onApartmentTitleClickMock}
         />
@@ -30,6 +31,6 @@ describe(`<ApartmentCard /> e2e suite`, () => {
     apartmentButton.simulate(`mouseEnter`, eventMock);
 
     expect(onApartmentCardHoverMock).toHaveBeenCalledTimes(1);
-    expect(onApartmentCardHoverMock.mock.calls[0][0]).toMatchObject(apartmentMock);
+    expect(onApartmentCardHoverMock.mock.calls[0][0]).toMatchObject(SINGLE_APARTMENT);
   });
 });
--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,17 +1,57 @@
-import React from 'react';
-import Main from './../main/main.jsx';
+import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import Main from './../main/main.jsx';
+import Property from '../property/property.jsx';
 
-const handleApartmentTitleClick = () => {};
+class App extends PureComponent {
+  constructor(props) {
+    super(props);
 
-const App = (props) => {
-  const {apartmentList} = props;
+    this.state = {
+      clickedProperty: null
+    };
 
-  return <Main
-    apartmentList={apartmentList}
-    onApartmentTitleClick={handleApartmentTitleClick}
-  />;
-};
+    this.handleApartmentTitleClick = this.handleApartmentTitleClick.bind(this);
+  }
+
+  handleApartmentTitleClick(prop) {
+    this.setState({clickedProperty: prop});
+  }
+
+  _renderMainScreen() {
+    const {apartmentList} = this.props;
+    const {clickedProperty} = this.state;
+
+    if (clickedProperty) {
+      return <Property
+        property={clickedProperty}
+      />;
+    }
+
+    return <Main
+      apartmentList={apartmentList}
+      onApartmentTitleClick={this.handleApartmentTitleClick}
+    />;
+  }
+
+  render() {
+    const {apartmentList} = this.props;
+
+    return <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          {this._renderMainScreen()}
+        </Route>
+        <Route exact path="/dev-property">
+          <Property
+            property={apartmentList[0]}
+          />
+        </Route>
+      </Switch>
+    </BrowserRouter>;
+  }
+}
 
 App.propTypes = {
   apartmentList: PropTypes.arrayOf(PropTypes.shape()).isRequired
