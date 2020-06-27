af56eb929871678c489bfc47d57ee1261729efa9
Anton Churkin <afrg@yandex.ru>
2020-06-27 15:50:02 +0500

    index.js has got redux store

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+
 import App from './components/app/app.jsx';
 import ApartmentContentMock from './mocks/offers.js';
 import CitiesMock from './mocks/cities.js';
+import reducer from './reducer/js';
+
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+);
 
 const init = () => {
   ReactDOM.render(
-      <App
-        apartmentList = {ApartmentContentMock}
-        cities = {CitiesMock}/>,
+      <Provider store={store}>
+        <App
+          apartmentList = {ApartmentContentMock}
+          cities = {CitiesMock}/>
+      </Provider>,
       document.querySelector(`#root`)
   );
 };
