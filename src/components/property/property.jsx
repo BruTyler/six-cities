30a842e50a6fb5103c6b6f0c24878c5da78de9ee
Anton Churkin <afrg@yandex.ru>
2020-06-30 12:42:26 +0500

    component Property includes component ReviewList

--- src/components/property/property.jsx
+++ src/components/property/property.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {PlaceType} from '../../const';
+import ReviewList from '../review-list/review-list';
 
 const Property = ({
   apartment: {
     type, description, rating, price, isPremium, isFavourite,
-    photoSet, bedrooms, adultsMax, goods, host
-  }
+    photoSet, bedrooms, adultsMax, goods, host, reviews
+  },
 }) => {
   let percentageRating = Math.round(Math.round(rating) / 5 * 100);
 
@@ -90,79 +91,7 @@ const Property = ({
               </p>
             </div>
           </div>
-          <section className="property__reviews reviews">
-            <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">1</span></h2>
-            <ul className="reviews__list">
-              <li className="reviews__item">
-                <div className="reviews__user user">
-                  <div className="reviews__avatar-wrapper user__avatar-wrapper">
-                    <img className="reviews__avatar user__avatar" src="img/avatar-max.jpg" width="54" height="54" alt="Reviews avatar" />
-                  </div>
-                  <span className="reviews__user-name">
-                  Max
-                  </span>
-                </div>
-                <div className="reviews__info">
-                  <div className="reviews__rating rating">
-                    <div className="reviews__stars rating__stars">
-                      <span style={{width: `80%`}}></span>
-                      <span className="visually-hidden">Rating</span>
-                    </div>
-                  </div>
-                  <p className="reviews__text">
-                  A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.
-                  </p>
-                  <time className="reviews__time" dateTime="2019-04-24">April 2019</time>
-                </div>
-              </li>
-            </ul>
-            <form className="reviews__form form" action="#" method="post">
-              <label className="reviews__label form__label" htmlFor="review">Your review</label>
-              <div className="reviews__rating-form form__rating">
-                <input className="form__rating-input visually-hidden" name="rating" value="5" id="5-stars" type="radio" />
-                <label htmlFor="5-stars" className="reviews__rating-label form__rating-label" title="perfect">
-                  <svg className="form__star-image" width="37" height="33">
-                    <use xlinkHref="#icon-star"></use>
-                  </svg>
-                </label>
-
-                <input className="form__rating-input visually-hidden" name="rating" value="4" id="4-stars" type="radio" />
-                <label htmlFor="4-stars" className="reviews__rating-label form__rating-label" title="good">
-                  <svg className="form__star-image" width="37" height="33">
-                    <use xlinkHref="#icon-star"></use>
-                  </svg>
-                </label>
-
-                <input className="form__rating-input visually-hidden" name="rating" value="3" id="3-stars" type="radio" />
-                <label htmlFor="3-stars" className="reviews__rating-label form__rating-label" title="not bad">
-                  <svg className="form__star-image" width="37" height="33">
-                    <use xlinkHref="#icon-star"></use>
-                  </svg>
-                </label>
-
-                <input className="form__rating-input visually-hidden" name="rating" value="2" id="2-stars" type="radio" />
-                <label htmlFor="2-stars" className="reviews__rating-label form__rating-label" title="badly">
-                  <svg className="form__star-image" width="37" height="33">
-                    <use xlinkHref="#icon-star"></use>
-                  </svg>
-                </label>
-
-                <input className="form__rating-input visually-hidden" name="rating" value="1" id="1-star" type="radio" />
-                <label htmlFor="1-star" className="reviews__rating-label form__rating-label" title="terribly">
-                  <svg className="form__star-image" width="37" height="33">
-                    <use xlinkHref="#icon-star"></use>
-                  </svg>
-                </label>
-              </div>
-              <textarea className="reviews__textarea form__textarea" id="review" name="review" placeholder="Tell how was your stay, what you like and what can be improved"></textarea>
-              <div className="reviews__button-wrapper">
-                <p className="reviews__help">
-                To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
-                </p>
-                <button className="reviews__submit form__submit button" type="submit" disabled="">Submit</button>
-              </div>
-            </form>
-          </section>
+          <ReviewList reviewList={reviews} />
         </div>
       </div>
       <section className="property__map map"></section>
@@ -290,6 +219,7 @@ Property.propTypes = {
       name: PropTypes.string.isRequired,
       isSuper: PropTypes.bool.isRequired,
     }),
+    reviews: PropTypes.array.isRequired,
   }),
 };
 
--- src/components/property/property.test.js
+++ src/components/property/property.test.js
@@ -22,6 +22,16 @@ const SINGLE_APARTMENT = {
     name: `name`,
     isSuper: true,
   },
+  reviews: [
+    {
+      id: 1,
+      authorName: `Max`,
+      authorAvatar: `img/avatar-max.jpg`,
+      rating: 4,
+      opinion: `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.`,
+      publishDate: `2014-05-20`,
+    },
+  ],
 };
 
 describe(`<Property /> render suit`, () => {
