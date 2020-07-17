bd9b44baf74f1ea4cdbe0b5b38353c6f67ac8168
Anton Churkin <afrg@yandex.ru>
2020-07-17 14:13:39 +0500

    component ReviewForm is detached from component ReviewList

---
+++ src/components/review-form/review-form.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import ReviewForm from './review-form.jsx';
+
+describe(`<ReviewForm /> render suit`, () => {
+  it(`<ReviewForm /> render list of reviews`, () => {
+    const generatedTree = renderer.create(
+        <ReviewForm
+          activeItem={``}
+        />
+    ).toJSON();
+
+    expect(generatedTree).toMatchSnapshot();
+  });
+});
